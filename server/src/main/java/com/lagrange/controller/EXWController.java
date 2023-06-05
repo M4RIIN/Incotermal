1809f002db6b06174d45bf5f8aff8f1da938f09a
Marin <nicolas.marin@keleo-solutions.com>
2023-06-05 21:28:16 +0200

    force

--- server/src/main/java/com/lagrange/controller/EXWController.java
+++ server/src/main/java/com/lagrange/controller/EXWController.java
@@ -1,29 +1,55 @@
 package com.lagrange.controller;
 
 import com.lagrange.controller.rest.CoutRevientMargeCommercialeRest;
-import org.lagrange.entity.Exw;
-import org.lagrange.entity.supplychain.SupplyChain;
+import org.lagrange.entity.exw.ExwCalculParameters;
+import org.lagrange.usecase.ExwUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class EXWController {
 
+    private final ExwUseCase exwUseCase;
+
+    public EXWController(ExwUseCase exwUseCase) {
+        this.exwUseCase = exwUseCase;
+    }
+
     @PostMapping("/api/cout-revient-marge-com")
-    public Double getInitialSupplyChain(@RequestBody CoutRevientMargeCommercialeRest rest){
-        return new Exw(
-                rest.getQuantite(),
-                rest.getCoutAchatMatierePremiere(),
-                rest.getMainOeuvre(),
-                rest.getAutreChargeVariables(),
-                rest.getChargesFixes(),
-                rest.getConditionnement(),
-                rest.getMargeCommercialeDepartUsine()
-        ).byCoutDeRevientEtMargeCommerciale();
+    public Double getCoutRevient(@RequestBody CoutRevientMargeCommercialeRest rest){
+        ExwCalculParameters exwCalculParameters = new ExwCalculParameters.Builder()
+                .setParametersForCalculCoutRevient(
+                        rest.getQuantite(),rest.getCoutAchatMatierePremiere(),
+                        rest.getMainOeuvre(),rest.getAutreChargeVariables(),
+                        rest.getChargesFixes(),rest.getConditionnement(),
+                        rest.getMargeCommercialeDepartUsine()
+                ).build();
+        return exwUseCase.getCoutDeRevient(exwCalculParameters);
+    }
+
+    @PostMapping("/api/vente-unitaire")
+    public Double getVenteUnitaire(@RequestBody CoutRevientMargeCommercialeRest rest){
+        ExwCalculParameters exwCalculParameters = new ExwCalculParameters.Builder()
+                .setParametersForCalculCoutRevient(
+                        rest.getQuantite(),rest.getCoutAchatMatierePremiere(),
+                        rest.getMainOeuvre(),rest.getAutreChargeVariables(),
+                        rest.getChargesFixes(),rest.getConditionnement(),
+                        rest.getMargeCommercialeDepartUsine()
+                ).build();
+        return exwUseCase.getCoutVenteUnitaire(exwCalculParameters);
+    }
+
+    @PostMapping("/api/vente-produit")
+    public Double getVenteProduit(@RequestBody CoutRevientMargeCommercialeRest rest){
+        ExwCalculParameters exwCalculParameters = new ExwCalculParameters.Builder()
+                .setParametersForCalculCoutRevient(
+                        rest.getQuantite(),rest.getCoutAchatMatierePremiere(),
+                        rest.getMainOeuvre(),rest.getAutreChargeVariables(),
+                        rest.getChargesFixes(),rest.getConditionnement(),
+                        rest.getMargeCommercialeDepartUsine()
+                ).build();
+        return exwUseCase.getCoutVenteProduit(exwCalculParameters);
     }
 }
